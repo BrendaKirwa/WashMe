65517eb9669a3c9cdfd4074b8503875ad715644f
Brendacherono <32226601+Brendacherono@users.noreply.github.com>
2018-06-08 18:18:20 +0300

    modified

--- app/src/main/java/com/example/dijonkariz/washme/SignIn.java
+++ app/src/main/java/com/example/dijonkariz/washme/SignIn.java
@@ -2,12 +2,28 @@ package com.example.dijonkariz.washme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignIn extends AppCompatActivity {
+
+    @BindView(R.id.username)EditText username;
+    @BindView(R.id.userEmail)EditText userEmail;
+    @BindView(R.id.userPassword)EditText userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.RegisterBtn)
+    void Register(){
+
     }
 }
---
+++ app/src/main/java/com/example/dijonkariz/washme/entities/AccessToken.java
@@ -0,0 +1,18 @@
+package com.example.dijonkariz.washme.entities;
+
+import com.squareup.moshi.Json;
+
+public class AccessToken {
+
+    @Json(name = "token_type")
+    String tokenType;
+
+    @Json(name = "expires_in")
+    String expiresIn;
+
+    @Json(name = "access_token")
+    String accessToken;
+
+    @Json(name = "refresh_token")
+    String refreshToken;
+}
